8effd46ad47debae3905e14bcd991147e372501d
davaughnwhite <1857342+davaughnwhite@users.noreply.github.com>
2022-10-31 21:55:19 -0400

    Add ratelimitter

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { PORT } from './config.js';
 import tokens from './routes/tokens/tokens.controller.js';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const app = express();
-
+app.use(limiter);
 app.use(express.json({ limit: '1MB' }));
 
 app.use('/tokens', tokens);
